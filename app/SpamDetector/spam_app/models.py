ea7361bb1f70cc4506aa43ce83d6db1d5739fed3
RoninNapoletano <74654834+RoninNapoletano@users.noreply.github.com>
2023-08-05 10:07:46 -0400

    Model EmailDate

--- app/SpamDetector/spam_app/models.py
+++ app/SpamDetector/spam_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
-# Create your models here.
+class EmailData(models.Model):
+    id = models.CharField(max_length=100)
+    email = models.EmailField()
+    created_at = models.DateField(auto_now_add=True)
+    predicted_label = models.IntegerField(choices=[(0, '0'), (1, '1')])

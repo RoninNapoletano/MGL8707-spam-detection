2f6836021222bd3dce0b6ccd5820b12475cb2fbe
RoninNapoletano <74654834+RoninNapoletano@users.noreply.github.com>
2023-08-05 13:13:40 -0400

    Optimize Models

--- app/SpamDetector/spam_app/models.py
+++ app/SpamDetector/spam_app/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 class EmailData(models.Model):
-    id = models.CharField(max_length=100)
+    id = models.CharField(max_length=100, primary_key=True)
     email = models.EmailField()
     created_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+class PredictedEmailData(EmailData):
+    predicted_label = models.IntegerField(choices=[(0, '0'), (1, '1')])
+
+class CorrectedPredictionEmailData(EmailData):
+    corrected_label = models.IntegerField(choices=[(0, '0'), (1, '1')])
     predicted_label = models.IntegerField(choices=[(0, '0'), (1, '1')])

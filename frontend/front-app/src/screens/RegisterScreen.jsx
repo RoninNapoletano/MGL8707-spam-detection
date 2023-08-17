a9ffeca40a72211e2a1563c1cb73b743c090326a
RoninNapoletano <74654834+RoninNapoletano@users.noreply.github.com>
2023-08-17 15:12:36 -0400

    Little update registrer

--- frontend/front-app/src/screens/RegisterScreen.jsx
+++ frontend/front-app/src/screens/RegisterScreen.jsx
@@ -91,7 +91,8 @@ export default function RegisterScreen() {
         onChangeText={text => {
           setEmail(text);
           setIsEmailInvalid(false);
-        }}        style={[
+        }}       
+        style={[
           styles.input,
         ]}
       />
